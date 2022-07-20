7857b04bfefec7e498b65b93928167f4b293643e
CHOI-K-ROCK <chl4842@gmail.com>
2022-07-20 14:40:35 +0900

    기본 설정 완료

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.section``;
+
+function App(): JSX.Element {
+  return (
+    <Container>
+      <div>App</div>
+    </Container>
+  );
+}
+
+export default App;
